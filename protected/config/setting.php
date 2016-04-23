4bb29541ee771c675516f682ec724dcd542a2af8
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-04-23 07:54:44 +0700

    ommu core: source

---
+++ protected/config/setting.php
@@ -0,0 +1,26 @@
+<?php
+return array(
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'webmaster@example.com',
+		'primaryLang' => 'id',
+		'translateLangs' => array(
+			'en' => 'en',
+			'id' => 'id',
+			'fr' => 'fr',
+		),
+		
+		// timthumb replace url
+		'timthumb_url_replace' => 0,		
+		'timthumb_url_replace_website' => 'http://nirwasita.com',	//default http
+		// access system *from product
+		'product_access_system' => 'sso.bpadjogja.info',
+		'oauth_server_options' => array(
+			//'http://ommu.co',
+			'http://localhost/_product_ommu.co',
+		),
+	),
+);
+?>
---
+++ protected/views/page/front_view.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Ommu Pages (ommu-pages)
+ * @var $this OmmuPagesController
+ * @var $model OmmuPages
+ * version: 1.1.0
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2012 Ommu Platform (ommu.co)
+ * @link https://github.com/oMMu/Ommu-Core
+ * @contact (+62)856-299-4114
+ *
+ */
+ 
+	$this->breadcrumbs=array(
+		'Ommu Pages'=>array('manage'),
+		$model->name,
+	);
+?>
+
+<?php if($model->media_show == 1) {
+	$images = Yii::app()->request->baseUrl.'/public/page/'.$model->media;
+	if($this->adsSidebar == true) {
+		if($model->media_type == 1)
+			echo '<img class="largemag" src="'.Utility::getTimThumb($images, 600, 900, 3).'" alt="">';
+		else
+			echo '<img class="mediummag" src="'.Utility::getTimThumb($images, 270, 500, 3).'" alt="">';
+	} else {
+		if($model->media_type == 1)
+			echo '<img class="largemag" src="'.Utility::getTimThumb($images, 1280, 1024, 3).'" alt="">';
+		else
+			echo '<img class="mediummag" src="'.Utility::getTimThumb($images, 270, 500, 3).'" alt="">';
+	}
+}?>
+
+<?php echo Phrase::trans($model->name, 2) != Utility::hardDecode(Phrase::trans($model->desc, 2)) ? Utility::cleanImageContent(Phrase::trans($model->desc, 2)) : '';?>
